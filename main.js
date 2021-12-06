2cebb145c5e0f8df47bf26a0974a192eca553ea0
auraz <TaartAndCake789>
2021-12-06 20:13:40 +0100

    Afwerking Front End

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ const challengeCourseInput = document.getElementById("challengeCourseInput")
 const challengePointsInput = document.getElementById("challengePointsInput")
 const loadChallengesBtn = document.getElementById("loadChallenges")
 loadChallengesBtn.onclick = getAllChallenges
+const editChallengeBtn = document.getElementById("editbtn")
+editChallengeBtn.onclick = updateChallenge
 
 
-async function insert() {
+async function insert(){
     let challengeName = challengeNameInput.value
     let challengeCourse = challengeCourseInput.value
     let challengePoints = challengePointsInput.value
@@ -16,25 +18,25 @@ async function insert() {
         "points": challengePoints < 0 ? 0 : challengePoints,
         "course": challengeCourse
     }
-    let result = await sendRequest("POST", "/api/insert", body)
+    let result = await sendRequest("POST","/challenges",body)
     clearForm()
     await getAllChallenges()
     return result
 }
 
-function clearForm() {
+function clearForm(){
     challengeNameInput.value = ""
     challengeCourseInput.value = ""
     challengePointsInput.value = ""
 }
 
-async function getAllChallenges() {
-    let result = await sendRequest("GET", "/api/challenges")
+async function getAllChallenges(){
+    let result = await sendRequest("GET","/challenges")
     addChallengesHtml(result)
 }
 
-async function sendRequest(type, endpoint, body) {
-    let response = await fetch(`${baseUrl}${endpoint}`, {
+async function sendRequest(type,endpoint,body){
+    let response = await fetch(`${baseUrl}${endpoint}`,{
         method: type,
         body: JSON.stringify(body),
         headers: {
@@ -45,26 +47,65 @@ async function sendRequest(type, endpoint, body) {
     return result
 }
 
-function addChallengesHtml(challenges) {
+function addChallengesHtml(challenges){
     let html = document.getElementById("challenges")
     html.innerHTML = ""
-    for (let challenge of challenges) {
+    for (let challenge of challenges){
         html.appendChild(createChallengeHtml(challenge))
     }
 }
 
-function editChallenge(id) {
-    console.log(id)
-    //TODO
+async function updateChallenge(){
+    let editNameInput = document.getElementById("challengeNameInputEdit")
+    let editCourseInput = document.getElementById("challengeCourseInputEdit")
+    let editPointsInput = document.getElementById("challengePointsInputEdit")
+    let challengeIdInput = document.getElementById("challengeId")
+
+    let body = {
+        "name": editNameInput.value,
+        "points": editPointsInput.value < 0 ? 0 : editPointsInput.value,
+        "course": editCourseInput.value
+    }
+    await sendRequest("PUT",`/challenges/${challengeIdInput.value}`,body)
+    hideEditForm()
+    getAllChallenges()
 }
 
-function deleteChallenge(id) {
-    console.log(id)
-    //TODO
+async function editChallenge(id){
+    let challenge = await sendRequest("GET",`/challenges/${id}`)
+    fillInEditForm(challenge)
 }
 
-function createChallengeHtml(challenge) {
+async function deleteChallenge(id){
+    await sendRequest("DELETE",`/challenges/${id}`)
+    await getAllChallenges()
+}
+
+function hideEditForm(){
+    let form = document.getElementById("editForm")
+    form.style.display = "None"
+}
+
+function showEditForm(){
+    let form = document.getElementById("editForm")
+    form.style.display = "block"
+}
+
+function fillInEditForm(challenge){
     console.log(challenge)
+    showEditForm()
+    let editNameInput = document.getElementById("challengeNameInputEdit")
+    let editCourseInput = document.getElementById("challengeCourseInputEdit")
+    let editPointsInput = document.getElementById("challengePointsInputEdit")
+    let challengeIdInput = document.getElementById("challengeId")
+
+    challengeIdInput.value = challenge._id
+    editNameInput.value = challenge.name
+    editPointsInput.value = challenge.points
+    editCourseInput.value = challenge.course
+}
+
+function createChallengeHtml(challenge){
     let container = document.createElement("div")
     let cardbody = document.createElement("div")
     let nameHtml = document.createElement("h3")
@@ -75,10 +116,10 @@ function createChallengeHtml(challenge) {
     let iconContainer = document.createElement("div")
     deleteIcon.classList = "far fa-trash-alt fa-2x delete"
     editIcon.classList = "far fa-edit fa-2x edit"
-    editIcon.onclick = function () {
+    editIcon.onclick = function(){
         editChallenge(challenge._id)
     }
-    deleteIcon.onclick = function () {
+    deleteIcon.onclick = function(){
         deleteChallenge(challenge._id)
     }
     iconContainer.appendChild(deleteIcon)
